06c007d0ea70ac8e77d0972b1faea1be2ca75312
D1stort19 <wangjiaqi02@qq.com>
2024-09-22 12:37:27 +0800

    update

---
+++ New.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int a[1000];
+int main()
+{
+    int n = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    return 0;
+}
